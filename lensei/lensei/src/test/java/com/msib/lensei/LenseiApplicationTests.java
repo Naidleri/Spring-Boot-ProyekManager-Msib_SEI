cd7e67cf2e893710e6289f2fe3eead153d41d267
Haris ardiansyah <111837825+Naidleri@users.noreply.github.com>
2024-08-15 16:45:14 +0700

    setup_project

---
+++ lensei/lensei/src/test/java/com/msib/lensei/LenseiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msib.lensei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenseiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
