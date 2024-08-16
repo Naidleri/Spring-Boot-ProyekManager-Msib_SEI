0771c8276b096871f98425d80cbe8ee8cfaf2bf6
haris ardian <harisfirman387@gmail.com>
2024-08-16 17:15:08 +0700

    Add Lokasi Controller and Lokasi Service for update and delete

--- lensei/src/main/java/com/msib/lensei/service/LokasiService.java
+++ lensei/src/main/java/com/msib/lensei/service/LokasiService.java
@@ -2,6 +2,7 @@ package com.msib.lensei.service;
 
 import com.msib.lensei.dto.LokasiDTO;
 import com.msib.lensei.entity.Lokasi;
+import com.msib.lensei.exception.ResourceNotFoundException;
 import com.msib.lensei.mapper.LokasiMapper;
 import com.msib.lensei.repository.LokasiRepository;
 import org.springframework.stereotype.Service;
@@ -30,5 +31,22 @@ public class LokasiService {
         return lokasiRepository.findAll().stream()
                 .map(lokasiMapper::toDto)
                 .collect(Collectors.toList());
+    }
+
+    public LokasiDTO updateLokasi(Integer id, LokasiDTO lokasiDto) {
+        Lokasi lokasi = lokasiRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Lokasi dengan id " + id + "tidak ditemukan"));
+        lokasi.setNamaLokasi(lokasiDto.getNamaLokasi());
+        lokasi.setNegara(lokasiDto.getNegara());
+        lokasi.setProvinsi(lokasiDto.getProvinsi());
+        lokasi.setKota(lokasiDto.getKota());
+        Lokasi updatedLokasi = lokasiRepository.save(lokasi);
+        return lokasiMapper.toDto(updatedLokasi);
+    }
+
+    public void deleteLokasi(Integer id) {
+        Lokasi lokasi = lokasiRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Lokasi dengan id  " + id + "tidak ditemukan"));
+        lokasiRepository.delete(lokasi);
     }
 }
